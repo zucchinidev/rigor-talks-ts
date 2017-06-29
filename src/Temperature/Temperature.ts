b133d6e9a98bff5f2c2ad5c9298c246ff53e9d40
Andrea Zucchini <azga84@gmail.com>
2017-06-29 20:06:14 +0200

    temperature  class

---
+++ src/Temperature/Temperature.ts
@@ -0,0 +1,22 @@
+import {TemperatureNegativeException} from './TemperatureNegativeException';
+
+export class Temperature {
+  constructor(private measure: number) {
+    this.setMeasure(measure);
+  }
+
+  getMeasure(): number {
+    return this.measure;
+  }
+
+  private setMeasure(measure: number) {
+    this.checkMeasureIsPositive();
+    this.measure = measure;
+  }
+
+  private checkMeasureIsPositive() {
+    if (this.measure < 0) {
+      throw new TemperatureNegativeException('Measure should be positive');
+    }
+  }
+}

4a1cc19633d2ae702171d086eb9562d5351b0f13
Andrea Zucchini <azga84@gmail.com>
2017-10-03 16:22:27 +0200

    add immutable decorator

--- src/Video/Application/Create/CreateVideoCommand.ts
+++ src/Video/Application/Create/CreateVideoCommand.ts
@@ -1,6 +1,8 @@
 import { Command } from '../../../Shared/Domain/Bus/Command/Command'
 import { Uuid } from '../../../Types/ValueObject/Uuid'
+import { Immutable } from '../../../Shared/Domain/Decorators/Immutable'
 
+@Immutable
 export class CreateVideoCommand extends Command {
   constructor (commandId: Uuid, private _id: string,
                private _title: string, private _url: string, private _courseId: string) {
